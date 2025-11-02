e8e9949d8d2d0f2d63fe0303d931f9bd116a35a5
cristian <cristian.beltran.paco@gmail.com>
2025-11-02 16:38:26 -0400

    push

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,9 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    host: true, // escucha en 0.0.0.0
+    port: 5173,
+    allowedHosts: [".local"],
+  },
 });
